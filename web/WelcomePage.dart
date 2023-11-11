0a82e7f9e12e7b65519ebd364a246000fd5026a6
jep180001 <112898626+jep180001@users.noreply.github.com>
2023-11-11 13:16:58 -0600

    Update WelcomePage.dart

--- web/WelcomePage.dart
+++ web/WelcomePage.dart
@@ -1,236 +1,263 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class WelcomePage extends StatelessWidget {
-  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: UserDashboard(),
+    );
+  }
+}
+
+class UserDashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
       appBar: AppBar(
+        title: Text('User Dashboard', style: GoogleFonts.playfairDisplay(fontSize: 30, fontWeight: FontWeight.w500)),
         backgroundColor: Colors.black,
-        elevation: 10,
-        actions: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: TextButton(
-              onPressed: () {
-                // Handle button press for "Button 1" here
-              },
-              child: Text(
-                'About',
-                style: GoogleFonts.roboto(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: TextButton(
-              onPressed: () {
-                // Handle button press for "Button 2" here
-              },
-              child: Text(
-                'Account',
-                style: GoogleFonts.roboto(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: TextButton(
-              onPressed: () {
-                // Handle button press for "Button 3" here
-              },
-              child: Text(
-                'Login',
-                style: GoogleFonts.roboto(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ),
-        ],
-        title: Align(
-          alignment: Alignment.topLeft,
-          child: TextButton(
-            onPressed: () {
-              // Handle button press for "TechAddict" here
-              Navigator.popUntil(context, (route) => route.isFirst);
-            },
-            child: Text(
-              'TechAddict',
-              style: GoogleFonts.playfairDisplay(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
       ),
-      body: DecoratedBox(
-        position: DecorationPosition.background,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,  // Adjust this value
-            end: Alignment.bottomCenter,  // Adjust this value
-            colors: [
-              desaturateColor(const Color.fromARGB(255, 147, 205, 253), 0.5),
-              desaturateColor(const Color.fromARGB(255, 144, 161, 255), 0.5),
-              desaturateColor(Color.fromARGB(255, 253, 191, 241), 0.5),
-              desaturateColor(Color.fromARGB(255, 255, 255, 255), 0.5),
-            ],
-          ),
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 200),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: EdgeInsets.only(left: 30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'WELCOME,',
-                        style: GoogleFonts.playfairDisplaySc(
-                          fontSize: 50,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      SizedBox(height: 16),
-                      Text(
-                        'We are excited you are here.',
-                        style: GoogleFonts.roboto(
-                          fontSize: 25,
-                          fontWeight: FontWeight.normal,
-                          color: Colors.black,
-                        ),
-                        textAlign: TextAlign.left,
-                      ),
-                    ],
-                  ),
-                ),
+      body: Stack(
+        children: [
+          // Gradient Background
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  desaturateColor(const Color.fromARGB(255, 147, 205, 253), 0.5),
+                  desaturateColor(const Color.fromARGB(255, 144, 161, 255), 0.5),
+                  desaturateColor(Color.fromARGB(255, 253, 191, 241), 0.5),
+                  desaturateColor(Color.fromARGB(255, 255, 255, 255), 0.5),
+                ],
               ),
-              SizedBox(height: 16),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: EdgeInsets.all(30.0),
-                  child: Container(
-                    width: 250.0,
-                    height: 50.0,
-                    child: ElevatedButton(
+            ),
+          ),
+          // Image
+          Positioned.fill(
+            child: Image.asset(
+              'assets/AdobeStock_523517509.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          // Content
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Welcome back!',
+                      style: GoogleFonts.playfairDisplay(fontSize: 45, fontWeight: FontWeight.bold),
+                    ),
+                    ElevatedButton(
                       onPressed: () {
-                        //Navigator.pushNamed(context, '/login');
-                        //Navigator.pushNamed(context, '/sign_up_page');
-                        _showSignUpLoginDialog(context);
+                        // Handle the click action for "Homepage"
+                        // You can navigate to a new screen or perform any other action.
+                        Navigator.pushNamed(context, '/welcome');
                       },
                       style: ElevatedButton.styleFrom(
+                        minimumSize: Size(100, 50),
                         primary: Colors.black,
-                        onPrimary: Colors.white,
-                        elevation: 30,
-                        side: BorderSide(color: Color.fromARGB(255, 0, 5, 9)),
+                        elevation: 10,
+                        side: BorderSide(color: Color.fromARGB(255, 0, 5, 9), width: 2),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(30.0),
                         ),
                       ),
-                      child: Text('GET STARTED'),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('HOMEPAGE', style: GoogleFonts.roboto(fontSize: 15)),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ),
-            ],
+                SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Navigate to details page
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(400, 80),
+                        primary: Colors.black,
+                        elevation: 10,
+                        side: BorderSide(color: Color.fromARGB(255, 0, 5, 9), width: 2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('DETAILS', style: GoogleFonts.roboto(fontSize: 20)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 50),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Navigate to the schedule call page
+                        Navigator.pushNamed(context, '/user_schedule_page');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(400, 80),
+                        primary: Colors.black,
+                        elevation: 10,
+                        side: BorderSide(color: Color.fromARGB(255, 0, 5, 9), width: 2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('YOUR SCHEDULE', style: GoogleFonts.roboto(fontSize: 20)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Handle the click action for "Your Daily Progress"
+                        // You can navigate to a new screen or perform any other action.
+                        Navigator.pushNamed(context, '/activity_begin');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(400, 300),
+                        primary: Colors.white,
+                        elevation: 10,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned.fill(
+                            child: Image.asset(
+                              'assets/AdobeStock_505636066.png', // Add your PNG here
+                              //fit: BoxFit.cover,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: 250,
+                                    width: 250,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'ACTIVITIES',
+                                        textAlign: TextAlign.center,
+                                        style: GoogleFonts.roboto(fontSize: 30, color: Colors.black, fontWeight: FontWeight.w500),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 20),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 50),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Handle the click action for "Your Daily Progress"
+                        // You can navigate to a new screen or perform any other action.
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(400, 300),
+                        primary: Colors.white,
+                        elevation: 10,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned.fill(
+                            child: Image.asset(
+                              'assets/AdobeStock_505636066.png', // Add your PNG here
+                              //fit: BoxFit.cover,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: 250,
+                                    width: 250,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'DAILY PROGRESS',
+                                        style: GoogleFonts.roboto(fontSize: 28, color: Colors.black, fontWeight: FontWeight.w500),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 20),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
-void _showSignUpLoginDialog(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        content: Container(
-          height: 750,
-          width: 750.0, // Set your desired width here
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.close), // Close icon
-                    onPressed: () {
-                      Navigator.pop(context); // Close the dialog
-                    },
-                  ),
-                ],
-              ),
-              SizedBox(height: 150),
-              Text(
-                'Join TechAddict',
-                style: GoogleFonts.playfairDisplay(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-              SizedBox(height: 50),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/sign_up_page');
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.black,
-                  onPrimary: Colors.white,
-                  elevation: 30,
-                  side: BorderSide(color: Color.fromARGB(255, 0, 5, 9)),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  fixedSize: Size(200, 60), // Set your desired button size here
-                ),
-                child: Text('SIGN UP', style: GoogleFonts.roboto(fontSize: 20)),
-              ),
-              SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/login');
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.black,
-                  onPrimary: Colors.white,
-                  elevation: 30,
-                  side: BorderSide(color: Color.fromARGB(255, 0, 5, 9)),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  fixedSize: Size(200, 60), // Set your desired button size here
-                ),
-                child: Text('LOGIN',  style: GoogleFonts.roboto(fontSize: 20)),
-              ),
-            ],
-          ),
-        ),
-      );
-    },
-  );
-}
-
-
 
   Color desaturateColor(Color color, double saturationFactor) {
     final HSLColor hslColor = HSLColor.fromColor(color);
